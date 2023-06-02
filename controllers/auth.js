849d5e71d4414493f6e0b7f04369bbeb87ea2008
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-02 20:38:56 +0530

    refactor: moved hashedPassword code to middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,13 +3,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
 async function register(req, res) {
-	const { name, email, password } = req.body;
-
-	const salt = await bcrypt.genSalt(10);
-	const hasedPassword = await bcrypt.hash(password, salt);
-	const tempUser = { name, email, password: hasedPassword };
-
-	const user = await User.create({ ...tempUser });
+	const user = await User.create({ ...req.body });
 	res.status(StatusCodes.CREATED).json({ user });
 }
 
