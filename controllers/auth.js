db6cab0c85d1fa873c67df9cfb2c8eedb26aa1bf
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-02 20:34:02 +0530

    feat: add hashedPassword feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,16 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 async function register(req, res) {
-	const user = await User.create({ ...req.body });
-	res.status(StatusCodes.CREATED).json(user);
+	const { name, email, password } = req.body;
+
+	const salt = await bcrypt.genSalt(10);
+	const hasedPassword = await bcrypt.hash(password, salt);
+	const tempUser = { name, email, password: hasedPassword };
+
+	const user = await User.create({ ...tempUser });
+	res.status(StatusCodes.CREATED).json({ user });
 }
 
 async function login(req, res) {
