d6303193eecd22bfa0f06ef232f2acc599993d4e
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-03 13:02:35 +0530

    refactor: migrate jwt function to user schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const jwt = require("jsonwebtoken");
 
 async function register(req, res) {
 	const user = await User.create({ ...req.body });
-	const token = jwt.sign({ userId: user._id }, "jwtSecret", {
-		expiresIn: "30d"
+	const token = user.createJWT();
+
+	res.status(StatusCodes.CREATED).json({
+		user: { name: user.name },
+		token
 	});
-	res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 }
 
 async function login(req, res) {
