4d295127bb888015733969041eaa6aa31f056d5a
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-03 12:51:32 +0530

    feat: add jwt tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,13 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 async function register(req, res) {
 	const user = await User.create({ ...req.body });
-	res.status(StatusCodes.CREATED).json({ user });
+	const token = jwt.sign({ userId: user._id }, "jwtSecret", {
+		expiresIn: "30d"
+	});
+	res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 }
 
 async function login(req, res) {
