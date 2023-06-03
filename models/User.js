5a23512d04eca52aeddf17d4baffa959745b247d
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-03 18:49:44 +0530

    refact: set jwt expiry using env

--- models/User.js
+++ models/User.js
@@ -33,7 +33,7 @@ UserSchema.pre("save", async function (next) {
 
 UserSchema.methods.createJWT = function () {
 	return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
-		expiresIn: "30d"
+		expiresIn: process.env.JWT_LIFETIME
 	});
 };
 
