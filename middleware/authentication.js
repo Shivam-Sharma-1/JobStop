3754eb1df0826c2f540258d9973cd2aed598c065
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-04 08:49:04 +0530

    feat: use authentication middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const UnauthenticatedError = require("../errors/unauthenticated");
+const User = require("../models/User");
 
 const authenticationMiddleware = async (req, res, next) => {
 	const authHeader = req.headers.authorization;
@@ -10,8 +11,11 @@ const authenticationMiddleware = async (req, res, next) => {
 	const token = authHeader.split(" ")[1];
 	try {
 		const payload = jwt.verify(token, process.env.JWT_SECRET);
-		const { userId, name } = payload;
-		req.user = { userId, name };
+		const user = User.findById(payload.id).select("-password");
+		req.user = user;
+
+		// const { userId, name } = payload;
+		// req.user = { userId, name };
 		next();
 	} catch (error) {
 		throw new UnauthenticatedError("Authentication Invalid");
