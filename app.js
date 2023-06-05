f70187f674eb4d97fe55ab2f2079f05c58ab4a59
Shivam77607 <shivamsharma77607@gmail.com>
2023-06-05 11:41:40 +0530

    build: add security packages

--- app.js
+++ app.js
@@ -14,12 +14,26 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const authenticationMiddleware = require("./middleware/authentication");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const RateLimit = require("express-rate-limit");
 
+app.set("trust proxy", 1);
+app.use(
+	RateLimit({
+		windowMs: 15 * 60 * 1000,
+		max: 100
+	})
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.get("/", (req, res) => {
-	res.send("jobs api");
+	res.send("<h1>Jobs API</h1>");
 });
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticationMiddleware, jobsRouter);
